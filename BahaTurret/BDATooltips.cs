42dc473ad16672cd1016e2248775e0332c71bdce
BahamutoD <paolo@encarnacion.org>
2015-08-27 17:20:31 -0400

    prep for eventual tooltips

---
+++ BahaTurret/BDATooltips.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BahaTurret
+{
+	public static class BDATooltips
+	{
+		public static string WM_RIPPLE
+		{
+			get
+			{
+				return "The ripple function allows you to fire missiles, rockets, or bombs at the configured rate by holding down the main fire key (" + BDInputSettingsFields.WEAP_FIRE_KEY.inputString + ")";
+			}
+		}
+
+
+	}
+}
+
