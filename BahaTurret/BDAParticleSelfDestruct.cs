50cfadaf848f084b5060bc4ac46ab1c0f30ae373
BahamutoD <paolo@encarnacion.org>
2015-12-02 19:22:46 -0500

    particle self destruct sequence for detached missile trails

---
+++ BahaTurret/BDAParticleSelfDestruct.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace BahaTurret
+{
+	public class BDAParticleSelfDestruct : MonoBehaviour
+	{
+		KSPParticleEmitter pEmitter;
+		BDAGaplessParticleEmitter gpe;
+
+		void Awake()
+		{
+			pEmitter = gameObject.GetComponent<KSPParticleEmitter>();
+			gpe = gameObject.GetComponent<BDAGaplessParticleEmitter>();
+		}
+
+		void Start()
+		{
+			StartCoroutine(SelfDestructRoutine());
+		}
+
+		IEnumerator SelfDestructRoutine()
+		{
+			pEmitter.emit = false;
+			if(gpe)
+			{
+				gpe.emit = false;
+			}
+			yield return new WaitForSeconds(pEmitter.maxEnergy);
+			Destroy(gameObject);
+			yield break;
+		}
+	}
+}
+
