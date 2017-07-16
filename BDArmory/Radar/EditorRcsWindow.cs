ca1578b82ede5b0048b17b64543fe954ed52cff1
TheDogKSP <cambrium@abwesend.de>
2017-07-16 19:25:56 +0200

    RCS shader and editor windowRect

---
+++ BDArmory/Radar/EditorRcsWindow.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using BDArmory.CounterMeasure;
+using BDArmory.Misc;
+using BDArmory.Parts;
+using BDArmory.Radar;
+using KSP.UI.Screens;
+using UnityEngine;
+using BDArmory.UI;
+
+namespace BDArmory.Radar
+{
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    class EditorRcsWindow : MonoBehaviour
+    {
+
+        public static EditorRcsWindow Instance = null;
+        private ApplicationLauncherButton toolbarButton = null;
+
+        private bool showRcsWindow = false;
+        private string windowTitle = "BDArmory Radar Cross Section Analysis";
+        private Rect windowRect = new Rect(300, 150, 650, 400);
+
+
+        void Awake()
+        {
+        }
+
+        void Start()
+        {
+            Instance = this;
+            AddToolbarButton();
+        }
+
+        private void OnDestroy()
+        {
+            if (toolbarButton)
+            {
+                ApplicationLauncher.Instance.RemoveModApplication(toolbarButton);
+                toolbarButton = null;
+            }
+        }
+
+        IEnumerator ToolbarButtonRoutine()
+        {
+            if (toolbarButton || (!HighLogic.LoadedSceneIsEditor)) yield break;
+            while (!ApplicationLauncher.Ready)
+            {
+                yield return null;
+            }
+
+            AddToolbarButton();
+        }
+
+        void AddToolbarButton()
+        {
+            if (HighLogic.LoadedSceneIsEditor)
+            {
+                if (toolbarButton is null)
+                {
+                    Texture buttonTexture = GameDatabase.Instance.GetTexture(BDArmorySettings.textureDir + "icon_rcs", false);
+                    toolbarButton = ApplicationLauncher.Instance.AddModApplication(ShowToolbarGUI, HideToolbarGUI, Dummy, Dummy, Dummy, Dummy, ApplicationLauncher.AppScenes.SPH | ApplicationLauncher.AppScenes.VAB, buttonTexture);
+                }
+            }
+        }
+
+        public void ShowToolbarGUI()
+        {
+            showRcsWindow = true;
+        }
+
+        public void HideToolbarGUI()
+        {
+            showRcsWindow = false;
+        }
+
+        void Dummy()
+        { }
+
+        void OnGUI()
+        {
+            if (showRcsWindow)
+            {
+               windowRect = GUI.Window(this.GetInstanceID(), windowRect, RcsWindow, windowTitle, HighLogic.Skin.window);
+               BDGUIUtils.UseMouseEventInRect(windowRect);
+            }
+        }
+
+        void RcsWindow(int windowID)
+        {
+
+            GUI.Label(new Rect(10, 40, 200, 20), "Frontal", HighLogic.Skin.box);
+            GUI.Label(new Rect(220, 40, 200, 20), "Lateral", HighLogic.Skin.box);
+            GUI.Label(new Rect(430, 40, 200, 20), "Dorsal",  HighLogic.Skin.box);
+
+            GUI.DragWindow();
+        }
+
+
+    } //EditorRCsWindow
+}
---
+++ BDArmory/Shaders/rcsShader.shader
@@ -0,0 +1,66 @@
+﻿// Upgrade NOTE: replaced 'mul(UNITY_MATRIX_MVP,*)' with 'UnityObjectToClipPos(*)'
+
+Shader "Custom/rcsShader" {
+
+	Properties{
+		_RCSCOLOR("RCSCOLOR", Color) = (1, 1, 1, 1)
+	}
+
+		SubShader{
+
+			Pass{
+
+			CGPROGRAM
+
+			// DEFINES
+			#pragma vertex RCSVertexShader
+			#pragma fragment RCSFragmentShader
+
+			// INCLUDES
+			#include "UnityCG.cginc"
+			#include "UnityLightingCommon.cginc"
+			#include "UnityStandardBRDF.cginc"
+
+
+			// uniforms from properties
+			fixed4 _RCSCOLOR;
+
+			struct VertexData {
+				float4 position : POSITION;
+				float3 normal : NORMAL;
+				float2 uv : TEXCOORD0;
+			};
+
+			struct Interpolators {
+				float4 position : SV_POSITION;
+				float3 normal : TEXCOORD1;
+			};
+
+
+
+			// Main Vertex Program
+			Interpolators RCSVertexShader(VertexData v) {
+				Interpolators i;
+				i.position = UnityObjectToClipPos(v.position);
+				i.normal = UnityObjectToWorldNormal(v.normal);
+				//i.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				return i;
+			}
+
+
+
+			// Main Fragment Program
+			float4 RCSFragmentShader(Interpolators i) : SV_TARGET{
+				float3 lightDir = _WorldSpaceLightPos0.xyz;
+				float3 lightColor = _RCSCOLOR.rgb;
+				float3 reflectionDir = reflect(-lightDir, i.normal);
+				return DotClamped(lightDir, reflectionDir);
+			}
+
+
+			ENDCG
+
+			} //PASS
+		} //SUBSHADER
+
+}
