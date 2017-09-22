cc8300df525112b44ffdbcaf6a7133d6d5f21a49
Jesus Rodriguez Valencia <jesus.rodriguez.valencia@gmail.com>
2017-09-22 19:58:47 +0200

    max damage setup

---
+++ BDArmory.Core/Behaviour/MaxDamageSetup.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace BDArmory.Core.Behaviour
+{
+
+    [KSPAddon(KSPAddon.Startup.Flight, false)]
+    public class MaxDamageSetup : MonoBehaviour
+    {
+        private int maxDamageMultiplier = 1000;
+        void Start()
+        {
+            GameEvents.onVesselCreate.Add(UpdateMaxDamage);
+            GameEvents.onVesselLoaded.Add(UpdateMaxDamage);    
+        }
+
+        private void UpdateMaxDamage(Vessel vessel)
+        {
+            List<Part>.Enumerator parts = vessel.parts.GetEnumerator();
+            while (parts.MoveNext())
+            {
+                if (parts.Current == null) continue;
+                parts.Current.maxTemp = Mathf.Clamp(parts.Current.mass * maxDamageMultiplier,3000,float.MaxValue);
+            }
+            parts.Dispose();
+        }
+
+        void OnDestroy()
+        {
+            GameEvents.onVesselCreate.Add(UpdateMaxDamage);
+            GameEvents.onVesselLoaded.Add(UpdateMaxDamage);
+        }
+    }
+}
