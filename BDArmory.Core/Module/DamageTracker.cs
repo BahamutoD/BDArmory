9ca04f2d8d67fbb387da5e904511ef4974578af5
Jesus Rodriguez Valencia <jesus.rodriguez.valencia@gmail.com>
2017-10-02 22:06:32 +0200

    rebalance of max damage

--- BDArmory.Core/Module/DamageTracker.cs
+++ BDArmory.Core/Module/DamageTracker.cs
@@ -29,7 +29,6 @@ namespace BDArmory.Core.Module
                 damageFieldFlight.minValue = 0f;
 
                 UI_ProgressBar damageFieldEditor = (UI_ProgressBar)Fields["Damage"].uiControlEditor;
-
                 damageFieldEditor.maxValue = CalculateMaxDamage();
                 damageFieldEditor.minValue = 0f;
                 this.part.RefreshAssociatedWindows();
@@ -42,7 +41,7 @@ namespace BDArmory.Core.Module
 
         private float CalculateMaxDamage()
         {
-            return maxDamageFactor * part.mass * Mathf.Clamp(part.crashTolerance, 1, 100);
+            return maxDamageFactor * Mathf.Clamp(part.mass,0.001f,50f) * Mathf.Clamp(part.crashTolerance, 1, 25);
         }
 
         public void DestroyPart()
