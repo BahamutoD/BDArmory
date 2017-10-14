9f3f5cdad1eafe0f0513eed705b5dfd7c8eb3b98
U-hades\dlane <dlane@rackspace.com>
2017-10-14 10:41:24 -0500

    set amrmor amount from BDArmor module once if set

---
+++ BDArmory.Core/Module/BDArmor.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace BDArmory.Core.Module
+{
+    public class BDArmor : PartModule
+    {
+        [KSPField(isPersistant = true)]
+        public float ArmorThickness = 0f;
+        
+        public override void OnStart(StartState state)
+        {
+            base.OnAwake();
+            part.force_activate();
+        }
+
+    }
+}
