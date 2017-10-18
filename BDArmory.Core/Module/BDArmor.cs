a5fbe8258fb2b73240ebca72efe3e526a0ae8bc5
U-hades\dlane <dlane@rackspace.com>
2017-10-17 21:27:44 -0500

    armor mass / volume approximation

--- BDArmory.Core/Module/BDArmor.cs
+++ BDArmory.Core/Module/BDArmor.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using KSPDev.ConfigUtils;
 
 namespace BDArmory.Core.Module
 {
@@ -10,16 +11,16 @@ namespace BDArmory.Core.Module
         [KSPField(isPersistant = true)]
         public float ArmorThickness = 0f;
 
-        [KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Area")]
-        public float PartArea = 0;
+        //[KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Area")]
+        //public float PartArea = 0;
 
-        [KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Area2")]
-        public float PartArea2 = 0;
+        //[KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Area2")]
+        //public float PartArea2 = 0;
 
         [KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Volume")]
         public float PartVolume = 0;
 
-        [KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Volume2")]
+        [KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "Part Volume with Armor")]
         public float PartVolume2 = 0;
 
         [KSPField(guiActive = true, guiActiveEditor = true, isPersistant = false, guiName = "ArmorMass")]
@@ -29,9 +30,9 @@ namespace BDArmory.Core.Module
         public static bool areacalc;
 
         [KSPEvent(guiActive = true, guiActiveEditor = true, guiName = "Calc Part")]
-        public void CalcArea()
+        public void doCalc()
         {
-            GetPartDetails();
+            SetPartMassByArmor();
         }
 
 
@@ -50,8 +51,7 @@ namespace BDArmory.Core.Module
         {
             base.OnAwake();
             part.force_activate();
-
-            //instance = this;
+            doCalc();
 
             switch (explodeMode)
             {
@@ -67,37 +67,41 @@ namespace BDArmory.Core.Module
             }
         }
 
-        public void GetPartDetails()
+        public void SetPartMassByArmor()
         {
-            PartArea = part.surfaceAreas.magnitude;
-            PartArea2 = GetPartArea(part.partInfo);
-            PartVolume = GetPartVolume(part.partInfo);
-            PartVolume2 = GetPartVolume_withArmor(part.partInfo);
-            ArmorMass = (8.05f * (PartVolume2 - PartVolume))/1000;
+            ArmorThickness = part.FindModuleImplementing<DamageTracker>().Armor;
+            //PartArea = part.surfaceAreas.magnitude;
+            //PartArea2 = GetPartArea(part.partInfo);
+            PartVolume = GetPartVolume(part.partInfo,part);
+            PartVolume2 = GetPartVolume_withArmor(part.partInfo,part);
+            ArmorMass = 8.05f * (PartVolume2 - PartVolume)/1000f;
+
+            part.mass += ArmorMass;
         }
 
-        public static float GetPartVolume(AvailablePart partInfo)
+        public static float GetPartVolume(AvailablePart partInfo,Part part)
         {
             var p = partInfo.partPrefab;
             float volume;
 
             var boundsSize = PartGeometryUtil.MergeBounds(p.GetRendererBounds(), p.transform).size;
-            volume = boundsSize.x * boundsSize.y * boundsSize.z * 1000f;            
+            volume = boundsSize.x * boundsSize.y * boundsSize.z * 10f;            
 
             // Apply cube of the scale modifier since volume involves all 3 axis.
-            return (float)(volume * Math.Pow(GetPartExternalScaleModifier(partInfo), 3));
+            return (float)(volume * Math.Pow(GetPartExternalScaleModifier(part), 3));
         }
 
-        public float GetPartVolume_withArmor(AvailablePart partInfo)
+        public float GetPartVolume_withArmor(AvailablePart partInfo,Part part)
         {
             var p = partInfo.partPrefab;
             float volume;
 
             var boundsSize = PartGeometryUtil.MergeBounds(p.GetRendererBounds(), p.transform).size;
-            volume = (boundsSize.x + (ArmorThickness/1000)) * boundsSize.y * boundsSize.z * 1000f;
+            volume = (boundsSize.x + (ArmorThickness/1000)) * boundsSize.y * boundsSize.z;
+            volume *= 10f;
 
             // Apply cube of the scale modifier since volume involves all 3 axis.
-            return (float)(volume * Math.Pow(GetPartExternalScaleModifier(partInfo), 3));
+            return (float)(volume * Math.Pow(GetPartExternalScaleModifier(part), 3));
         }
 
         public static float GetPartArea(AvailablePart partInfo)
@@ -109,35 +113,32 @@ namespace BDArmory.Core.Module
             area = 2 * (boundsSize.x * boundsSize.y) + 2 * (boundsSize.y * boundsSize.z) + 2 * (boundsSize.x * boundsSize.z);
                        
             return area;
-        }
-        
+        }        
 
-        public static float GetPartExternalScaleModifier(ConfigNode partNode)
+        public static float GetPartExternalScaleModifier(Part part)
         {
-            // TweakScale compatibility.
-            foreach (var node in partNode.GetNodes("MODULE"))
-            {
-                if (node.GetValue("name") == "TweakScale")
-                {
-                    double defaultScale = 1.0f;
-                    defaultScale = float.Parse(node.GetValue("defaultScale"));
-                    //ConfigAccessor.GetValueByPath(node, "defaultScale", ref defaultScale);
-                    double currentScale = 1.0f;
-                    //ConfigAccessor.GetValueByPath(node, "currentScale", ref currentScale);
-                    currentScale = float.Parse(node.GetValue("currentScale"));
+            double defaultScale = 1.0f;
+            double currentScale = 1.0f;
 
+            if (part.Modules.Contains("TweakScale"))
+            {
+                PartModule pM = part.Modules["TweakScale"];
+                if (pM.Fields.GetValue("currentScale") != null)
+                {
+                   try
+                    {
+                        defaultScale = pM.Fields.GetValue<float>("defaultScale");
+                        currentScale = pM.Fields.GetValue<float>("currentScale");
+                    }
+                    catch
+                    {
+                        
+                    }
                     return (float)(currentScale / defaultScale);
                 }
             }
             return 1.0f;
         }
-
-        public static float GetPartExternalScaleModifier(AvailablePart avPart)
-        {
-            return GetPartExternalScaleModifier(avPart.partConfig);
-        }
-
-
 
     }
 }
