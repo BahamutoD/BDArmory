3a42114845309c42508ded7c9cb522bdbad8394a
Jesus Rodriguez Valencia <jesus.rodriguez.valencia@gmail.com>
2018-04-21 12:08:31 +0200

    Revert "chaff fixed"
    This reverts commit 6dd0ddb7ff855f1064739f69751b9d67201b51f3.

--- BDArmory/CounterMeasure/CMChaff.cs
+++ BDArmory/CounterMeasure/CMChaff.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using BDArmory.FX;
+﻿using System.Collections;
 using BDArmory.Misc;
-using BDArmory.UI;
 using UnityEngine;
 
 namespace BDArmory.CounterMeasure
@@ -11,20 +7,13 @@ namespace BDArmory.CounterMeasure
     public class CMChaff : MonoBehaviour
     {
         KSPParticleEmitter pe;
-        public Vessel sourceVessel;
-        Vector3 relativePos;
+
 
         const float drag = 5;
 
         Vector3d geoPos;
         Vector3 velocity;
         CelestialBody body;
-
-        Vector3 upDirection;
-        public bool alive = true;
-        float startTime;
-
-        float lifeTime = 5;
 
         public void Emit(Vector3 position, Vector3 velocity)
         {
@@ -37,15 +26,8 @@ namespace BDArmory.CounterMeasure
         {
             if (!pe)
             {
-               
                 pe = gameObject.GetComponentInChildren<KSPParticleEmitter>();
-
-                pe.emit = true;
-                lifeTime = pe.maxEnergy;
-                var main = pe.ps.main;
-
-                main.emitterVelocityMode = ParticleSystemEmitterVelocityMode.Transform;
-                main.maxParticles = 1;
+                EffectBehaviour.AddParticleEmitter(pe);
             }
 
             body = FlightGlobals.currentMainBody;
@@ -54,87 +36,32 @@ namespace BDArmory.CounterMeasure
                 gameObject.SetActive(false);
                 return;
             }
-            upDirection = VectorUtils.GetUpDirection(transform.position);
-            startTime = Time.time;
+
+            StartCoroutine(LifeRoutine());
         }
 
-        void FixedUpdate()
+        IEnumerator LifeRoutine()
         {
-            if (!gameObject.activeInHierarchy)
+            geoPos = VectorUtils.WorldPositionToGeoCoords(transform.position, body);
+
+            pe.EmitParticle();
+
+            float startTime = Time.time;
+            while (Time.time - startTime < pe.maxEnergy)
             {
-                return;
+                transform.position = body.GetWorldSurfacePosition(geoPos.x, geoPos.y, geoPos.z);
+                velocity += FlightGlobals.getGeeForceAtPosition(transform.position)*Time.fixedDeltaTime;
+                Vector3 dragForce = (0.008f)*drag*0.5f*velocity.sqrMagnitude*
+                                    (float)
+                                    FlightGlobals.getAtmDensity(FlightGlobals.getStaticPressure(transform.position),
+                                        FlightGlobals.getExternalTemperature(), body)*velocity.normalized;
+                velocity -= (dragForce)*Time.fixedDeltaTime;
+                transform.position += velocity*Time.fixedDeltaTime;
+                geoPos = VectorUtils.WorldPositionToGeoCoords(transform.position, body);
+                yield return new WaitForFixedUpdate();
             }
 
-            if (velocity != Vector3.zero)
-            {
-                transform.rotation = Quaternion.LookRotation(velocity, upDirection);
-            }
-
-
-            //Particle effects
-            Vector3 downForce = Vector3.zero;
-            //downforce
-
-            if (sourceVessel != null)
-            {
-                downForce = (Mathf.Clamp((float)sourceVessel.srfSpeed, 0.1f, 150) / 150) *
-                            Mathf.Clamp01(20 / Vector3.Distance(sourceVessel.transform.position, transform.position)) * 20 *
-                            -upDirection;
-            }
-
- 
-            pe.worldVelocity = 2 * ParticleTurbulence.flareTurbulence + downForce;
-           
-
-            //floatingOrigin fix
-            if (sourceVessel != null)
-            {
-                if (((transform.position - sourceVessel.transform.position) - relativePos).sqrMagnitude > 800 * 800)
-                {
-                    transform.position = sourceVessel.transform.position + relativePos;
-                }
-
-                relativePos = transform.position - sourceVessel.transform.position;
-            }
-            //
-
-
-            if (Time.time - startTime > lifeTime) //stop emitting after lifeTime seconds
-            {
-                alive = false;
-
-                this.pe.emit = false;
-            }
-
-
-            if (Time.time - startTime > lifeTime + 11) //disable object after x seconds
-            {
-                BDArmorySetup.numberOfParticleEmitters--;
-                gameObject.SetActive(false);
-                return;
-            }
-
-
-            //physics
-            //atmospheric drag (stock)
-            float simSpeedSquared = velocity.sqrMagnitude;
-            Vector3 currPos = transform.position;
-            float mass = 0.001f;
-            float drag = 1f;
-            Vector3 dragForce = (0.008f * mass) * drag * 0.5f * simSpeedSquared *
-                                (float)
-                                FlightGlobals.getAtmDensity(FlightGlobals.getStaticPressure(currPos),
-                                    FlightGlobals.getExternalTemperature(), FlightGlobals.currentMainBody) *
-                                velocity.normalized;
-
-            velocity -= (dragForce / mass) * Time.fixedDeltaTime;
-            //
-
-            //gravity
-            if (FlightGlobals.RefFrameIsRotating)
-                velocity += FlightGlobals.getGeeForceAtPosition(transform.position) * Time.fixedDeltaTime;
-
-            transform.position += velocity * Time.fixedDeltaTime;
+            gameObject.SetActive(false);
         }
     }
 }
